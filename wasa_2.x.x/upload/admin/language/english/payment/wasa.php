df6692445112176def57e5fad9932a954cfc81d7
David Villa <david@kfoobar.se>
2021-04-29 12:16:58 +0200

    Update english translation

--- wasa_2.x.x/upload/admin/language/english/payment/wasa.php
+++ wasa_2.x.x/upload/admin/language/english/payment/wasa.php
@@ -1,51 +1,25 @@
 <?php
-// Heading
-$_['heading_title']		 = 'wasakredit.se';
 
-// Text
-$_['text_edit']		   = 'Edit wasa';
-$_['text_payment']		= 'Payment';
-$_['text_extension']		= 'Extension';
-$_['text_payments']		= 'Payments';
-$_['text_success']		= 'Success: You have modified Wasa Payment Module!';
-$_['text_next']			= 'Next';
-$_['text_live']			= 'Live (Production)';
-$_['text_test']			= 'Test (Sandbox)';
-$_['text_wasa']			= '<a href="//wasakredit.se" target="_blank"><img src="view/image/payment/wasa.png" alt="wasakredit.se" title="wasakredit.se" style="border: 1px solid #EEEEEE;" /></a>';
+// Heading
+$_['heading_title']      = 'Wasa Kredit';
 
 // Tab
-$_['tab_general']			= 'General';
-$_['tab_secret_test']	= 'Test Secret';
-$_['tab_secret_live']	= 'Live Secret';
+$_['tab_settings']       = 'Settings';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified Wasa Kredit Payment module!';
+$_['text_edit']          = 'Edit Wasa Kredit';
 
 // Entry
-$_['entry_test_mode'] = 'Test More';
+$_['entry_client_id']    = 'Client ID';
+$_['entry_secret_key']   = 'Secret Key';
+$_['entry_test_mode']    = 'Test Mode';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_status']       = 'Status';
 
-$_['entry_status'] = 'Status';
+// Logo
+$_['text_wasa']          = '<img src="view/image/payment/wasa.png" alt="Wasa Kredit" title="Wasa Kredit" style="border: 1px solid #EEEEEE; height: 35px;" />';
 
-$_['entry_sort_order'] = 'Sort Order';
-
-$_['entry_order_success_status'] = 'Order Success Status';
-$_['entry_order_success_status_help'] = 'Order status that will set for Successful Payment';
-
-$_['entry_order_failed_status'] = 'Order Failed Status';
-$_['entry_order_failed_status_help'] = 'Order status that will set for Failed Payment';
-
-
-$_['entry_store_card'] = 'Store Customer Card';
-$_['entry_store_card_help'] = 'Enable this if you want to allow customers to save their cards.';
-
-$_['entry_test_public_key'] = 'Test Public Key';
-$_['entry_test_public_key_help'] = 'Public Key for Sandbox Accuont';
-
-$_['entry_test_secret_key'] = 'Test Secret Key';
-$_['entry_test_secret_key_help'] = 'Secret Key for Sandbox Accuont';
-
-$_['entry_live_public_key'] = 'Live Public Key';
-$_['entry_live_public_key_help'] = 'Public Key for Production Accuont';
-
-$_['entry_live_secret_key'] = 'Live Secret Key';
-$_['entry_live_secret_key_help'] = 'Secret Key for Production Accuont';
-
-$_['entry_debug'] = 'Debug';
-
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment Wasa Kredit Payment!';
