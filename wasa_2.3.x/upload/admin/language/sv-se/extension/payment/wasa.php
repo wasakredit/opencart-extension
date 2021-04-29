2f130d632200eacd20d9b71a3c962fbc622c4297
David Villa <david@kfoobar.se>
2021-04-29 12:17:34 +0200

    Add swedish translations

---
+++ wasa_2.3.x/upload/admin/language/sv-se/extension/payment/wasa.php
@@ -0,0 +1,25 @@
+<?php
+
+// Heading
+$_['heading_title']      = 'Wasa Kredit';
+
+// Tab
+$_['tab_settings']       = 'Inställningar';
+
+// Text
+$_['text_extension']     = 'Moduler';
+$_['text_success']       = 'Klart: Dina inställningar för Wasa Kredit har nu uppdaterats!';
+$_['text_edit']          = 'Ändra Wasa Kredit';
+
+// Entry
+$_['entry_client_id']    = 'KlientId';
+$_['entry_secret_key']   = 'API-nyckel';
+$_['entry_test_mode']    = 'Testläge';
+$_['entry_order_status'] = 'Orderstatus';
+$_['entry_status']       = 'Status';
+
+// Logo
+$_['text_wasa']          = '<img src="view/image/payment/wasa.png" alt="Wasa Kredit" title="Wasa Kredit" style="border: 1px solid #EEEEEE; height: 35px;" />';
+
+// Error
+$_['error_permission']   = 'Varning: Du har inte rättigheter att ändra inställningar för Wasa Kredit!';
