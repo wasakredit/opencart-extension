ebe1a68edd1e05a041c7d9be9739e554d81a0758
David Villa <david@kfoobar.se>
2021-05-24 20:16:03 +0200

    Add translations

--- upload/catalog/language/en-gb/extension/payment/wasa.php
+++ upload/catalog/language/en-gb/extension/payment/wasa.php
@@ -2,3 +2,7 @@
 
 // Text
 $_['text_title'] = 'Wasa Kredit';
+$_['text_test_mode'] = 'Wasa Kredit is in test mode!';
+
+// Error
+$_['error_currency'] = 'Wasa Kredit is only available for orders with the currency SEK';
--- upload/catalog/language/sv-se/extension/payment/wasa.php
+++ upload/catalog/language/sv-se/extension/payment/wasa.php
@@ -2,3 +2,7 @@
 
 // Text
 $_['text_title'] = 'Wasa Kredit';
+$_['text_test_mode'] = 'Wasa Kredit är i testläge!';
+
+// Error
+$_['error_currency'] = 'Wasa Kredit är bara tillgänglig för beställningar med valutan SEK';
