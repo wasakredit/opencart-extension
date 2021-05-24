f3a8ee30a19fd7fcbe09cfafd4ae1f416f5bda92
David Villa <david@kfoobar.se>
2021-05-24 20:06:04 +0200

    Update SDK

---
+++ upload/system/library/wasa/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.2.x-dev',
+    'version' => '1.2.9999999.9999999-dev',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'db20ec2a8522d1243b2455d9defcc56cc21a230f',
+    'name' => 'wasa/opencart-extension',
+  ),
+  'versions' => 
+  array (
+    'wasa/client-php-sdk' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '5e36a430694e7f1d3d73c72968ee5d960744b306',
+    ),
+    'wasa/opencart-extension' => 
+    array (
+      'pretty_version' => '1.2.x-dev',
+      'version' => '1.2.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db20ec2a8522d1243b2455d9defcc56cc21a230f',
+    ),
+  ),
+);
