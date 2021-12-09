8454286be8bdc3e2e9bae0a791584f0a0832d4f8
David Villa <david@kfoobar.se>
2021-12-09 16:15:19 +0100

    Update Composer packages

--- upload/system/library/wasa/composer/installed.php
+++ upload/system/library/wasa/composer/installed.php
@@ -1,34 +1,34 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => '1.2.x-dev',
-    'version' => '1.2.9999999.9999999-dev',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../../../',
+        'aliases' => array(),
+        'reference' => '03acd2fd731f40096ba977e4e1807b8657f0b0b6',
+        'name' => 'wasa/opencart-extension',
+        'dev' => true,
     ),
-    'reference' => 'db20ec2a8522d1243b2455d9defcc56cc21a230f',
-    'name' => 'wasa/opencart-extension',
-  ),
-  'versions' => 
-  array (
-    'wasa/client-php-sdk' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '5e36a430694e7f1d3d73c72968ee5d960744b306',
+    'versions' => array(
+        'wasa/client-php-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wasa/client-php-sdk',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '5e36a430694e7f1d3d73c72968ee5d960744b306',
+            'dev_requirement' => false,
+        ),
+        'wasa/opencart-extension' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../../../',
+            'aliases' => array(),
+            'reference' => '03acd2fd731f40096ba977e4e1807b8657f0b0b6',
+            'dev_requirement' => false,
+        ),
     ),
-    'wasa/opencart-extension' => 
-    array (
-      'pretty_version' => '1.2.x-dev',
-      'version' => '1.2.9999999.9999999-dev',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'db20ec2a8522d1243b2455d9defcc56cc21a230f',
-    ),
-  ),
 );
