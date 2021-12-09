72b55b41aed3bd6cfe90cafec29bfdad717b7c0e
David Villa <david@kfoobar.se>
2021-12-09 16:19:32 +0100

    Rewrite widget for monthly cost. Use OCMod to load modifications for the widget.

---
+++ upload/catalog/model/extension/module/wasa.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once(DIR_SYSTEM . 'library/wasa/wasa/client-php-sdk/Wasa.php');
+
+class ModelExtensionModuleWasa extends Model
+{
+    public function getWidget($product, $format = 'small')
+    {
+        if (!$this->config->get('payment_wasa_show_widget')) {
+            return;
+        }
+
+        $this->client = Sdk\ClientFactory::CreateClient(
+            $this->config->get('payment_wasa_client_id'),
+            $this->config->get('payment_wasa_secret_key'),
+            $this->config->get('payment_wasa_test_mode')
+        );
+
+        $price = !empty($product['special']) ? $product['special'] : $product['price'];
+        $price = $this->tax->calculate($price, $product['tax_class_id'], $this->config->get('config_tax'));
+
+        $response = $this->client->get_monthly_cost_widget($price, $format);
+
+        return $this->load->view('extension/module/wasa', [
+            'snippet' => $response->data ?? null
+        ]);
+    }
+}
